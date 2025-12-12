24f05ff4f6b1490248404bc426b68c6280c03d5f
isabelysrocha6-senai <isabely.s.rocha6@aluno.senai.br>
2025-12-12 01:32:51 -0300

    Add files via upload

---
+++ Atividade-11-12/Materia.cs
@@ -0,0 +1,11 @@
+// ...existing code...
+using System;
+
+namespace Atividade_11_12
+{
+    public class Materia
+    {
+        public string Nome { get; set; }
+        public double Nota { get; set; }
+    }
+}
---
+++ Atividade-11-12/Program.cs
@@ -0,0 +1,39 @@
+﻿// ...existing code...
+using System;
+using System.Collections.Generic;
+
+namespace Atividade_11_12
+{
+    class Program
+    {
+        static void Main()
+        {
+            var materias = new List<Materia>
+            {
+                new Materia { Nome = "Matemática", Nota = 9.5 },
+                new Materia { Nome = "Português", Nota = 8.7 },
+                new Materia { Nome = "Física", Nota = 9.8 },
+            };
+
+            double soma = 0;
+            double maiorNota = double.MinValue;
+            Materia materiaMaior = null;
+
+            foreach (var m in materias)
+            {
+                soma += m.Nota;
+                if (m.Nota > maiorNota)
+                {
+                    maiorNota = m.Nota;
+                    materiaMaior = m;
+                }
+            }
+
+            Console.WriteLine();
+            if (materiaMaior != null)
+                Console.WriteLine($"Maior nota entre todas as matérias: {maiorNota} ({materiaMaior.Nome})");
+            else
+                Console.WriteLine("Nenhuma matéria informada.");
+        }
+    }
+}
